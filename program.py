a1f9ade63340a383f95ebc81b128dd81735be004
Isek7 <bengabrielisek@gmail.com>
2023-10-31 12:00:22 +0100

    Update

---
+++ program.py
@@ -0,0 +1,16 @@
+a = 23
+b = 44
+c = 75
+# test expression
+if a > b:
+   if b > c:
+# statement to be run
+        print("a is greater than b and b is greater c")
+   else:
+        print("a is greater than b and less than c")
+    
+elif a == b:
+    print("a is equall to b")
+
+else:
+    print("a is less than b")
